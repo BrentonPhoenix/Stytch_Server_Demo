f902152afc56e865104562430bd1c4a9f0d962f0
BrentonPhoenix <brentonphoenix@gmail.com>
2022-09-26 16:42:30 -0400

    fixed the error, I forgot that I had to configure the dotenv package at the top. Well it's fixed now so I can finally finish this

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+const Express = require('express')
+const app = Express()
 const cors = require('cors')
 const stytch = require('stytch')
 
 app.use(cors())
-app.use(express.json())
+app.use(Express.json())
 
 const client = new stytch.Client({
     project_id: process.env.Project_ID,
     secret: process.env.Stytch_Secret,
     env: stytch.envs.test,
 })
+
 
 app.post('/login', async (req, res)=>{
     let email = req.body.email
@@ -21,11 +23,31 @@ app.post('/login', async (req, res)=>{
     }
     const response = await client.magicLinks.email.loginOrCreate(params)
     console.log(client.project_id)
-    res.json(response)
+    res.json(response) })
    
     
-})
+
 
 app.listen(3005, ()=>{
     console.log("Server is running")
 })
+
+
+// const params = {
+//     email: REDACTED
+// }
+
+// client.magicLinks.email.loginOrCreate(params)
+// .then(resp=> console.log(resp))
+// .catch(err=> console.log(err))
+
+// router.post('/login', async (req,res) =>{
+//     let email = req.body.email
+//     const params = {
+//         email,
+//         login_magic_link_url: 'http://localhost:3000/auth',
+//          signup_magic_link_url: 'http://localhost:3000/auth'
+//     }
+//     const response = await client.magicLinks.email.loginOrCreate(params)
+//     res.json(response)
+// })
