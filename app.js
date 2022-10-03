53703f318a23c40224d8256b3fc8bfef4ba89555
BrentonPhoenix <brentonphoenix@gmail.com>
2022-10-03 00:21:20 -0400

    thought i had a big error but it was just a typo in the client side

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ const client = new stytch.Client({
     env: stytch.envs.test,
 })
 
+const authMiddleware = (req, res, next) => {
+    const sessionToken = req.headers.sessiontoken 
+    client.sessions
+    .authenticate({session_token: sessionToken})
+    .then(()=> {next()})
+    .catch((err)=>{
+        res.status(401).json(err)
+        console.log(err)
+    })
+}
+
 
 app.post('/login', async (req, res)=>{
     let email = req.body.email
@@ -22,32 +33,28 @@ app.post('/login', async (req, res)=>{
         signup_magic_link_url: 'http://localhost:3000/auth'
     }
     const response = await client.magicLinks.email.loginOrCreate(params)
-    console.log(client.project_id)
+    console.log(client)
     res.json(response) })
    
-    
+app.post('/auth', async (req,res)=>{
+    try {
+    const token = req.body.token
+    const sessionToken = await client.magicLinks.authenticate(token, {
+        session_duration_minutes: 180,
+    })
+    console.log(sessionToken)
+    res.json(sessionToken)}
+    catch(err){
+        res.json(err)
+    }
+})
 
+
+app.post("/test", authMiddleware ,(req,res)=>{
+    res.json("IT WORKED, THIS USER IS AUTHENTICATED!!!")
+})
 
 app.listen(3005, ()=>{
     console.log("Server is running")
 })
 
-
-// const params = {
-//     email: REDACTED
-// }
-
-// client.magicLinks.email.loginOrCreate(params)
-// .then(resp=> console.log(resp))
-// .catch(err=> console.log(err))
-
-// router.post('/login', async (req,res) =>{
-//     let email = req.body.email
-//     const params = {
-//         email,
-//         login_magic_link_url: 'http://localhost:3000/auth',
-//          signup_magic_link_url: 'http://localhost:3000/auth'
-//     }
-//     const response = await client.magicLinks.email.loginOrCreate(params)
-//     res.json(response)
-// })
